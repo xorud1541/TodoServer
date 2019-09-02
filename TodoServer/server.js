cd86aa6daaf348b6edb00fb2351bb5b47c05e1c7
전태경 <xorud1544@naver.com>
2019-09-02 22:32:28 +0900

    3000 -> 80포트변경, 로그인결과 로그 추가

--- TodoServer/server.js
+++ TodoServer/server.js
@@ -4,17 +4,17 @@ var app = express();
 app.get('/users/:password', function (req, res) {
     var pw = req.params.password;
     if (pw == "123") {
-        res.status(200).json();
+        res.status(200).json({'code' : 200});
         var ip = req.connection.address();
         console.log(ip);
         console.log('success');
     }
     else {
-        res.status(400).json();
+        res.status(400).json({ 'code' : 400});
         console.log('fail');
     }
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.listen(80, function () {
+    console.log('server is running ... ');
 });
